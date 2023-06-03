eaf58eede69a66fd30c7b26d21ee5a1205fbbd90
Andrés <axy09179@gmail.com.com>
2023-06-03 03:50:39 -0500

    modal y setTimeOut

--- main.js
+++ main.js
@@ -44,26 +44,49 @@
       correct: function correct(){
         input.style.borderColor="#14f708"
       },
-      modalError: function modalError(){
+      // modalError: function modalError(){
         //  window.open('/modalError.html', '_blank');
-        document.getElementById("modalError").click();
-      },
+        // document.getElementById("modalError").click();
+        // Swal.fire({
+        //   icon: "error",
+        //   title: "Oops...",
+        //   text: "El email debe de ser válido, y el campo no debe de estar vacío",
+        //   showConfirmButton: false,
+        //   timer: 4000,
+        // });
+      // },
       modalExito: function modalExito(){
         // window.open('/modalExito.html', '_blank');
         // window.location.href = ('/modalExito.html', '_blank');
-        document.getElementById("modalExito").click();
+        // document.getElementById("modalExito").click();
+        Swal.fire({
+          icon: "success",
+          title: "Correo válido",
+          text: "Validación de email confirmada",
+          showConfirmButton: false,
+          timer: 4000,
+        }); 
      },
+  
+
+ 
     };
 
-  
+    
+    // let parrafo = document.querySelector("#mensajes");
     let email = document.querySelector(".input").value;
+    // let botonModal = document.getElementById("btn-modal").checked=true
+    // botonModal.checked == true;
     let erroresEncontrados = [];
 
     //validar campo vacío
-    if (!email) {
+    let regex = /^\s*$/
+ 
+    if (!email==="") {
       erroresEncontrados.push(errores.vacio);
       errores.error()
-      errores.modalError()
+      // errores.modalError()
+      abrirmodal()
     }
 
     // Validar email
@@ -71,18 +94,23 @@
     if (!emailRegex.test(email)) {
       erroresEncontrados.push(errores.email);
       errores.error()
-      errores.modalError()
+      // errores.modalError()
+      abrirmodal()
     }
 
     if (erroresEncontrados.length > 0) {
       console.log("Se encontraron los siguientes errores:");
       for (let i = 0; i < erroresEncontrados.length; i++) {
         console.log("- " + erroresEncontrados[i]);
+        console.log(erroresEncontrados)
+        document.querySelector("#mensajes").innerHTML=`${erroresEncontrados}`
       }
     } else {
       errores.correct()
-     errores.modalExito()
+      // errores.modalExito()
+      abrirmodal()
       console.log("Los datos son válidos. ¡Registro exitoso!");
+      document.querySelector("#mensajes").innerHTML=`Los datos son válidos. ¡Registro exitoso!`
       
     }
     return erroresEncontrados;
@@ -94,8 +122,23 @@
     validarDatos();
   });
     
+  
+    let btnCerrarModal = document.querySelector("#btn-cerrar-modal");
+    btnCerrarModal.addEventListener("click", ()=>{
+     modal.close()  
+    })
 
-
+ 
+    
+   
+    let abrirmodal= function abrirModal(){
+      let modal = document.querySelector("#modal");
+      modal.showModal()
+      setTimeout(function(){
+        modal.close()
+      }, 3000)
+     }
+     
 
 
 
---
+++ main2.js
@@ -0,0 +1,10 @@
+const btnAbrirModal = document.querySelector("btn_valida");
+      const btnCerrarModal = document.querySelector("#btn-cerrar-modal");
+      const modal = document.querySelector("#modal");
+    //   btnAbrirModal.addEventListener("click", ()=>{
+    //     modal.showModal()
+    //   })
+
+    //   btnCerrarModal.addEventListener("click", ()=>{
+    //     modal.close()
+    //   })
