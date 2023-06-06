4990184da172aad97a09b4f9778331f01df0a721
Andrés <axy09179@gmail.com.com>
2023-06-05 22:04:37 -0500

    últimos retoques

--- main.js
+++ main.js
@@ -15,14 +15,13 @@
 //         }
 // })
 
-
 //validación con función
 // function encuentraMiPerro(datos) {
 //     if(datos.includes("@")){
 //         alert("bien");
 //     }else{
 //         alert("mal");
-//     } 
+//     }
 // }
 
 // let button = document.querySelector("#btn_valida");
@@ -32,116 +31,162 @@
 //     encuentraMiPerro(input)
 // })
 
-  
-  // validación de datos con uso de función, objetos y regex 
-  function validarDatos() {
-    const errores = {
-      email: "El email es inválido",
-      vacio: "El campo no puede estar vacío",
-      error: function error(){
-        input.style.borderColor="red"
-      },
-      correct: function correct(){
-        input.style.borderColor="#14f708"
-      },
-      // modalError: function modalError(){
-        //  window.open('/modalError.html', '_blank');
-        // document.getElementById("modalError").click();
-        // Swal.fire({
-        //   icon: "error",
-        //   title: "Oops...",
-        //   text: "El email debe de ser válido, y el campo no debe de estar vacío",
-        //   showConfirmButton: false,
-        //   timer: 4000,
-        // });
-      // },
-      modalExito: function modalExito(){
-        // window.open('/modalExito.html', '_blank');
-        // window.location.href = ('/modalExito.html', '_blank');
-        // document.getElementById("modalExito").click();
-        Swal.fire({
-          icon: "success",
-          title: "Correo válido",
-          text: "Validación de email confirmada",
-          showConfirmButton: false,
-          timer: 4000,
-        }); 
-     },
-  
+//abrir ventanas en nuevas pestañas
+// modalError: function modalError(){
+//     window.open('/modalError.html', '_blank');
+//    document.getElementById("modalError").click();
+//  },
 
- 
-    };
+// validación de datos con uso de función, objetos y regex
+function validarDatos2(input) {
+  const errores = {
+    email: "El email es inválido",
+    vacio: "El campo no puede estar vacío",
+    error: function error() {
+      input.style.borderColor = "red";
+    },
+    correct: function correct() {
+      input.style.borderColor = "#14f708";
+    },
+  };
 
-    
-    // let parrafo = document.querySelector("#mensajes");
-    let email = document.querySelector(".input").value;
-    // let botonModal = document.getElementById("btn-modal").checked=true
-    // botonModal.checked == true;
-    let erroresEncontrados = [];
+  let nombre = document.querySelector("#input2").value;
+  let erroresEncontrados = [];
 
-    //validar campo vacío
-    let regex = /^\s*$/
- 
-    if (!email==="") {
-      erroresEncontrados.push(errores.vacio);
-      errores.error()
-      // errores.modalError()
-      abrirmodal()
-    }
+  //validación campo vacío nombre y validez campo email
+  let regex = /^\s*$/;
+  if (!nombre) {
+    input.style.borderColor = "red";
+    erroresEncontrados.push(errores.vacio);
+    console.log("nombre y email vacios");
+  } else {
+    input.style.borderColor = "#14f708";
+  }
+  if (email == /^\s*$/) {
+    erroresEncontrados.push(errores.vacio);
+    erroresEncontrados.push(errores.email);
 
-    // Validar email
-    const emailRegex = /^\S+@\S+\.\S+$/;
-    if (!emailRegex.test(email)) {
-      erroresEncontrados.push(errores.email);
-      errores.error()
-      // errores.modalError()
-      abrirmodal()
-    }
-
-    if (erroresEncontrados.length > 0) {
-      console.log("Se encontraron los siguientes errores:");
-      for (let i = 0; i < erroresEncontrados.length; i++) {
-        console.log("- " + erroresEncontrados[i]);
-        console.log(erroresEncontrados)
-        document.querySelector("#mensajes").innerHTML=`${erroresEncontrados}`
-      }
-    } else {
-      errores.correct()
-      // errores.modalExito()
-      abrirmodal()
-      console.log("Los datos son válidos. ¡Registro exitoso!");
-      document.querySelector("#mensajes").innerHTML=`Los datos son válidos. ¡Registro exitoso!`
-      
-    }
-    return erroresEncontrados;
+    abrirmodal();
+    document.querySelector("#tituloModal").innerHTML = "Campo email Vacido";
+  }
+  if (email && !nombre) {
+    abrirmodal();
   }
 
-  let button = document.querySelector("#btn_valida");
-  button.addEventListener("click", (e) => {
-    e.preventDefault();
-    validarDatos();
-  });
-    
-  
-    let btnCerrarModal = document.querySelector("#btn-cerrar-modal");
-    btnCerrarModal.addEventListener("click", ()=>{
-     modal.close()  
-    })
+  // Validar email
+  const emailRegex = /^\S+@\S+\.\S+$/;
+  if (!emailRegex.test(email)) {
+    erroresEncontrados.push(errores.email);
+    errores.error();
+    abrirmodal();
+    document.querySelector("#tituloModal").innerHTML =
+      "Campo Email Vacido o Incorrecto";
+  } else {
+    errores.correct();
+  }
 
- 
-    
-   
-    let abrirmodal= function abrirModal(){
-      let modal = document.querySelector("#modal");
-      modal.showModal()
-      setTimeout(function(){
-        modal.close()
-      }, 3000)
-     }
-     
+  //   regex para validar vacío /^\s*$/
+  if (erroresEncontrados.length > 0) {
+    console.log("Se encontraron los siguientes errores:");
+    for (let i = 0; i < erroresEncontrados.length; i++) {
+      console.log("- " + erroresEncontrados[i]);
+      console.log(erroresEncontrados);
+      document.querySelector("#mensajes").innerHTML = `${erroresEncontrados}`;
+    }
+  } else {
+    abrirmodal();
+    console.log("Los datos son válidos. ¡Registro exitoso!");
+    document.querySelector(
+      "#mensajes"
+    ).innerHTML = `Los datos son válidos. ¡Registro exitoso!`;
+    document.querySelector("#tituloModal").innerHTML = "Felicitaciones";
+  }
+  return erroresEncontrados;
+}
 
+function validarDatos() {
+  const errores = {
+    email: "El email es inválido",
+    vacio: "El campo no puede estar vacío",
+    error: function error() {
+      input.style.borderColor = "red";
+    },
+    correct: function correct() {
+      input.style.borderColor = "#14f708";
+    },
+  };
 
+  let email = document.querySelector("#input").value;
+  let nombre = document.querySelector("#input2").value;
+  let erroresEncontrados = [];
 
-  
-  
+  //validación campo vacío nombre y validez campo email
+  let regex = /^\s*$/;
 
+  if (!nombre) {
+    erroresEncontrados.push(errores.vacio);
+
+    console.log("nombre y email vacios");
+  } else {
+  }
+  if (email == /^\s*$/) {
+    erroresEncontrados.push(errores.vacio);
+    erroresEncontrados.push(errores.email);
+
+    abrirmodal();
+    document.querySelector("#tituloModal").innerHTML = "Campo email Vacido";
+  }
+  if (email && !nombre) {
+    abrirmodal();
+  }
+
+  // Validar email
+  const emailRegex = /^\S+@\S+\.\S+$/;
+  if (!emailRegex.test(email)) {
+    erroresEncontrados.push(errores.email);
+    errores.error();
+    abrirmodal();
+    document.querySelector("#tituloModal").innerHTML =
+      "Campo Email Vacido o Incorrecto";
+  } else {
+    errores.correct();
+  }
+
+  //   regex para validar vacío /^\s*$/
+  if (erroresEncontrados.length > 0) {
+    console.log("Se encontraron los siguientes errores:");
+    for (let i = 0; i < erroresEncontrados.length; i++) {
+      console.log("- " + erroresEncontrados[i]);
+      console.log(erroresEncontrados);
+      document.querySelector("#mensajes").innerHTML = `${erroresEncontrados}`;
+    }
+  } else {
+    abrirmodal();
+    console.log("Los datos son válidos. ¡Registro exitoso!");
+    document.querySelector(
+      "#mensajes"
+    ).innerHTML = `Los datos son válidos. ¡Registro exitoso!`;
+    document.querySelector("#tituloModal").innerHTML = "Felicitaciones";
+  }
+  return erroresEncontrados;
+}
+
+let button = document.querySelector("#btn_valida");
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  validarDatos();
+  validarDatos2(input2);
+});
+
+let btnCerrarModal = document.querySelector("#btn-cerrar-modal");
+btnCerrarModal.addEventListener("click", () => {
+  modal.close();
+});
+
+let abrirmodal = function abrirModal() {
+  let modal = document.querySelector("#modal");
+  modal.show();
+  setTimeout(function () {
+    modal.close();
+  }, 3000);
+};
